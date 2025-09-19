ed94f216931aa0d0c943713a262ef080a3858225
Adhil <adhilaaza@gmail.com>
2025-09-20 00:18:14 +0530

    npm loaded

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold">Welcome to MEDCHAT</h1>
+            <p className="mt-4 text-lg text-gray-600">
+                Your virtual medical card solution
+            </p>
+        </div>
+    );
+}
