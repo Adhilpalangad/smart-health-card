cfe4ade0a5cc03af9fac04c87e9a5199514144c9
Adhil <adhilaaza@gmail.com>
2025-09-20 00:27:25 +0530

    tailwind added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,9 +2,12 @@ export default function LandingPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-4xl font-bold">Welcome to MEDCHAT</h1>
-            <p className="mt-4 text-lg text-gray-600">
+            <p className="mt-4 text-lg text-red-600">
                 Your virtual medical card solution
             </p>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 }
